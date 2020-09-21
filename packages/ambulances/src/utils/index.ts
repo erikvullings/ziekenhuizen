4708cea7f930f8c5687f9f0783d612f47cca75a3
Erik Vullings <erik.vullings@gmail.com>
2020-09-21 11:17:21 +0200

    Added sleep function

--- packages/ambulances/src/utils/index.ts
+++ packages/ambulances/src/utils/index.ts
@@ -2,3 +2,5 @@ const pcFormatter = /(\d{4})\s*(\w{2})/gm;
 
 export const formatPC = (pc?: string) =>
   pc && pcFormatter.test(pc) ? pc.replace(pcFormatter, '$1 $2').toUpperCase() : pc;
+
+export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
