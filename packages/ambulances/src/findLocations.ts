2a3ad1bfbf93cc1581830e098fdc99e1c79daf78
Erik Vullings <erik.vullings@gmail.com>
2022-02-04 17:10:03 +0100

    :lipstick:

--- packages/ambulances/src/findLocations.ts
+++ packages/ambulances/src/findLocations.ts
@@ -1,27 +1,31 @@
-import fs from 'fs';
-import path from 'path';
-import * as papa from 'papaparse';
-import { pdokLocationSvc } from './services';
-import { IAddress, IAmbulancePost } from './models';
+import fs from "fs";
+import path from "path";
+import * as papa from "papaparse";
+import { pdokLocationSvc } from "./services";
+import { IAddress, IAmbulancePost } from "./models";
 
-const filename = path.join(process.cwd(), './data/ambulancestandplaatsen.csv');
-const outputFile = path.join(process.cwd(), './data/ambulancestandplaatsen.json');
+const filename = path.join(process.cwd(), "./data/ambulancestandplaatsen.csv");
+const outputFile = path.join(
+  process.cwd(),
+  "./data/ambulancestandplaatsen.json"
+);
 
 const run = async () => {
-  return new Promise((resolve) => {
-    let file = fs.readFileSync(filename, 'utf8');
+  return new Promise<void>((resolve) => {
+    let file = fs.readFileSync(filename, "utf8");
     if (file.charCodeAt(0) === 0xfeff) {
       file = file.substr(1);
     }
     papa.parse<IAmbulancePost>(file, {
-      delimiter: ';',
+      delimiter: ";",
       header: true,
       complete: async (results) => {
         const ambulanceposten = results.data.reduce((acc, cur) => {
           if (!acc.hasOwnProperty(cur.id)) {
             acc[cur.id] = {} as IAmbulancePost;
           }
-          const { Standplaats, Straatnaam, Beschikbaarheid, Postcode, Plaats } = cur;
+          const { Standplaats, Straatnaam, Beschikbaarheid, Postcode, Plaats } =
+            cur;
           if (Standplaats) acc[cur.id].Standplaats = Standplaats;
           if (Straatnaam) {
             const extractHuisnummer = /([\w0-9 .]*) ([\dA-Za-z\-]*)$/;
@@ -49,8 +53,10 @@ const run = async () => {
           ambulancepost.lon = loc?.lon;
           output.push(ambulancepost);
         }
-        console.log('Finished:', JSON.stringify(output, null, 2));
-        fs.writeFileSync(outputFile, JSON.stringify(output, null, 2), { encoding: 'utf8' });
+        console.log("Finished:", JSON.stringify(output, null, 2));
+        fs.writeFileSync(outputFile, JSON.stringify(output, null, 2), {
+          encoding: "utf8",
+        });
         resolve();
       },
     });
